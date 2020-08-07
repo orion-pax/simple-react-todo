e53ed6ffbbbb3a3f2fea7994c93426173e8afb90
Tafadzwa Chimberengwa <orion-pax@users.noreply.github.com>
2020-08-07 18:26:31 +0200

    Updates

---
+++ precache-manifest.d2a74a7dc6bed4813078afe43506f73b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d402299621623cfded9b18fa4758ccc",
+    "url": "/simple-react-todo/index.html"
+  },
+  {
+    "revision": "6e8440bc1d672cb95453",
+    "url": "/simple-react-todo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "541bf2c1fae095604135",
+    "url": "/simple-react-todo/static/js/2.fb03929f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-react-todo/static/js/2.fb03929f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e8440bc1d672cb95453",
+    "url": "/simple-react-todo/static/js/main.931fcdc2.chunk.js"
+  },
+  {
+    "revision": "6472e1252be2bd57f955",
+    "url": "/simple-react-todo/static/js/runtime-main.46c95cfb.js"
+  }
+]);
